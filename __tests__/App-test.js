22aed869e06aed9d41e45abd3ffb631990fce731
bshwach <34045556+bshwach@users.noreply.github.com>
2019-05-28 23:24:50 -0500

    Add files via upload

---
+++ __tests__/App-test.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import NavigationTestUtils from 'react-navigation/NavigationTestUtils';
+import App from '../App';
+
+describe('App snapshot', () => {
+  jest.useFakeTimers();
+  beforeEach(() => {
+    NavigationTestUtils.resetInternalState();
+  });
+
+  it('renders the loading screen', async () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
